821862e473abbe39fb664cec88ab1faffb3f4a24
Neil Prajapati <neilp0101@gmail.com>
2019-09-11 18:32:55 -0700

    React website build 1

---
+++ precache-manifest.2bd1780ac16e8578dd8e4e5a84cc75f1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b7c633477d52501b1f5c892aed4acbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12bd07e64da4f47203a6",
+    "url": "/static/css/main.eebbb0eb.chunk.css"
+  },
+  {
+    "revision": "d8bfcff730312674172d",
+    "url": "/static/js/2.3722daac.chunk.js"
+  },
+  {
+    "revision": "12bd07e64da4f47203a6",
+    "url": "/static/js/main.20b09493.chunk.js"
+  },
+  {
+    "revision": "560ba80bb29eed1ad7e1",
+    "url": "/static/js/runtime~main.29359674.js"
+  },
+  {
+    "revision": "678fb88e7d8f08877d443fd475057bed",
+    "url": "/static/media/dblogo.678fb88e.png"
+  },
+  {
+    "revision": "74f7cdffc9b2c3f62771c7acac3f512d",
+    "url": "/static/media/grey.74f7cdff.png"
+  },
+  {
+    "revision": "8bcc224c56cfe0b904c120e76c58d615",
+    "url": "/static/media/me.8bcc224c.png"
+  },
+  {
+    "revision": "972be12a256999bbfe1f980134e0d240",
+    "url": "/static/media/r4.972be12a.jpg"
+  }
+]);
